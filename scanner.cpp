ac11ffd958fa55a7bf97434611a858c557d0dd65
Ronald Pepmeyer <ronaldpepmeyer@Ronalds-MacBook-Air.local>
2016-09-20 21:40:06 -0500

    Prints tokens with line number, ignores comment blocks

--- scanner.cpp
+++ scanner.cpp
@@ -1,33 +1,61 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <ctype.h>
 
 using namespace std;
 
-enum tokenType {foo, bar}; //Needs filled out
-
-/* Node for list of tokens--probably unnecessary
-struct tokenNode {
-  string token;
-  tokenType type;
-  unsigned long line;
-  tokenNode* next;
-};
-*/
+// Takes a character as an argument
+// Returns TRUE if character matches a self deliminating token.
+bool isSelfDeliminator(char c) {
+  char selfDeliminators[] = "+-*/<=(),:{;}";
+  for(int i = 0; i < 13; i++) {
+    if (c == selfDeliminators[i]) return true;
+  }
+  return false;
+}
 
 int main(int argc, char *argv[]) {
-  // Declarations
-  unsigned long line = 1;
-  string accum = "";
-  char currentChar;
+  unsigned long line = 1; // line number currently reading from
+  string accum = "";      // characters read in of current token so far
+  char currentChar;       // the current character being read
+  bool isComment = false; // TRUE if scanning a comment
   // Opening file
   ifstream inputFile;
   inputFile.open(argv[1]);
-  inputFile.get(currentChar);
   // Scanner loop
-  while(!inputFile.eof()) {
-    cout << currentChar;
+  while(1) {
     inputFile.get(currentChar);
+    if (!isComment) { // Skip to ELSE if reading a comment
+      if (inputFile.eof()) { // End of file -- breaks loop
+        break;
+      }
+      else if (isspace(currentChar)) { // Found white space
+        if (accum.length() > 0) { // If full, accum is token
+          cout << line << " <UNKNOWN> " << accum << '\n';
+        }
+        accum = "";
+        if (currentChar == '\n') line++; // increment line count 
+      }
+      else if (isSelfDeliminator(currentChar)) { // Found deliminator
+        if (accum.length() > 0) { // If full, accum is token
+          cout << line << " <UNKNOWN> " << accum << '\n';
+        }
+        accum = "";
+        if (currentChar != '{') { // Current char is a token
+          cout << line << " <UNKNOWN> " << currentChar << '\n';
+        }
+        else { // '{' indicates the beginning of a comment
+          isComment = true;
+        }
+      }
+      else { // Finally, the "default" case
+        accum += currentChar;
+      }
+    }
+    else { // Reading a comment block
+      if (currentChar == '}') isComment = false; // End comment
+    }
   }
   // Close file
   inputFile.close();
