da8c0acef09da7e96452087d09aa61952a5fd11c
Ronald Pepmeyer <ronaldpepmeyer@Ronalds-MacBook-Air.local>
2016-10-06 15:34:30 -0500

    Add basics for parser

---
+++ compiler.hpp
@@ -0,0 +1,57 @@
+#include <string>
+
+const int MAX_IDENT_LENGTH = 255;
+
+enum tokenType { op, lessthan, equals, leftparen, rightparen, 
+separator, terminator, comma, invalid, type, booleanvalue, booleanop, 
+keyword, number, identifier, punctuation, eos, program, definitions, 
+def, formals, nonemptyformals, nonemptyformalsprime, formal, body, 
+statementlist, typent, expr, exprprime, simpleexpr, simpleexprprime, 
+term, termprime, factor, factorprime, actuals};
+/*
+enum nonTerminalTokenType {program, definitions, def, formals, 
+nonemptyformals, nonemptyformals-prime, formal, body, statement-list, 
+type, expr, expr-prime, simple-expr, simple-expr-prime, term, 
+term-prime, factor, factor-prime, actuals};
+*/
+class token {
+  public:
+    std::string value;
+    enum tokenType type;
+    unsigned long line;
+};
+
+class tokenList {
+  public:
+    token item;
+    tokenList *next;
+    token peek() { // Returns token from next node in list
+      return (*next).item;
+    }
+    void advance() { // Replaces self with next node in list
+      item = (*next).item;
+      next = (*next).next;
+    }
+};
+
+class tokenStack {
+    tokenList *top;
+  public:
+    void push (token);
+    token pop ();
+};
+
+void tokenStack::push(token input) {
+  tokenList *next = top;
+  top = new tokenList;
+  top->item = input;
+  top->next = next;
+}
+
+token tokenStack::pop() {
+  tokenList *toFree = top;
+  token result = top->item;
+  top = top->next;
+  delete toFree;
+  return result;
+}
---
+++ parser.cpp
@@ -0,0 +1,22 @@
+#include <string>
+
+bool parser(tokenList * input) {
+  tokenList stream = *input;        // Input stream!
+  token currentToken = stream.item; // Current token!
+  tokenStack parserStack;           // Parser stack!
+  token eosToken = make("", eos, 0); 
+  token startToken = make("", program, 0);
+  // Step 1
+  parserStack.push(eosToken); // Push end of stream symbol
+  // Step 2
+  parserStack.push(startToken); // Push start symbol
+  // Step 3
+  while (currentToken.type != eos) {
+    //TODO: all the logic
+
+    //Advance stream
+    stream.advance();
+    currentToken = stream.item;
+  }
+  return false; // Obviously wrong placeholder return
+}
