4f04ed9bf7bdf48da87ea909fe4905523ff0da7f
rpepmeyer <pepmeyer@uni.edu>
2016-10-19 22:45:56 -0500

    formal parameters

--- ast-utilities.cpp
+++ ast-utilities.cpp
@@ -7,5 +7,8 @@ struct number_ast     {unsigned long value;};
 struct boolean_ast    {bool          value;};
 struct operator_ast   {char          value;};
 
-
+struct formal_ast  {
+  identifier_ast identifier;
+  type_ast       type;
+};
 
