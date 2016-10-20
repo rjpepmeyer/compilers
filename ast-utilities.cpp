e6f53909e550476dd6b878859d9ba79bca7ca2e9
DMNemec <devin.nemec@gmail.com>
2016-10-20 00:55:13 -0500

    #3
    Started if_statement. Thinking that we should make these classes so we
    can use polymorphism instead of void pointers to be able to add nodes
    them. That would also help prevent us from making pointers to node types
    where they shouldn't be.

--- ast-utilities.cpp
+++ ast-utilities.cpp
@@ -12,3 +12,28 @@ struct formal_ast  {
   type_ast       type;
 };
 
+struct if-ast {
+  leftChild      then_ast;// then expression node
+  rightChild     else_ast;// else expression node
+};
+
+struct then-ast {
+    leftChild    expr_ast;//expr with boolean result
+    rightChild   expr_ast;//expr to perform if L-child is true
+};
+
+struct else-ast {
+    leftChild    expr_ast;//expr with boolean result
+    rightChild   expr_ast;//expr to perform if L-child is false
+};
+
+// Struct Prototype
+struct /*Name*/ {
+  //(Values)*
+};
+
+// Binary Prototype
+struct /*name*/_ast {
+    leftChild    /*node*/_ast;
+    rightChild   /*node*/_ast;
+};
