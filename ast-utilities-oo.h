cdad3752e6c0962aa8716fc0aa7b72666ee8831c
DMNemec <devin.nemec@gmail.com>
2016-10-20 20:39:04 -0500

    #5
    node types

--- ast-utilities-oo.h
+++ ast-utilities-oo.h
@@ -5,52 +5,90 @@ class Node {
 	//abstract class prototype
 };
 
-class program_Node: public Node {
-	public string          identifier;
-	public body            Node;
-	public definitions[]   def_Node;
-    public formals[]	   formal_Node;
+class program_Node : public Node {
+	public identifier_Node   identifier;
+	public Node              body;
+	public def_Node[]        definitions;
+    public formal_Node[]     formal;
 };
 
-class def_Node: public program_Node {
-	//it's all the same!
-}
+class formal_Node : public Node: {
+  public identifier_Node     identifier;
+  public type_Node           type;
+};
 
+class def_Node : public Node {
+	public type_Node         type;
+};
+
+class body_Node : public Node {
+	public statement_Node[]  statements;
+};
+
+class statement_Node : public Node {
+	public print_Node        print;
+	public return_Node       returnVal;
+};
+
+class print_Node : public Node {
+	public expr_Node_Type    exprOne;
+};
+
+class return_Node : public print_Node {
+	//nothing new
+};
+
+class if_Node : public print_Node {
+	public expr_Node_Type    exprTwo;
+	public expr_Node_Type    exprThree;
+};
+
+class not_Node : public print_Node {
+	//nothing new
+}; 
+
+class function_Call_Node : public Node {
+    public identifier_Node   identifier;
+	public expr_Node_Type[]  expresions;
+};
+
+class negate_Node : public print_Node {
+	//nothing new
+};
+
+class block_Node : public print_Node {
+	//nothing new
+};
+
+class binary_Expr_Node : public print_Node {
+	public expr_Node_Type    exprTwo;
+	public operator_Node     op;
+};
+
+class expr_Node_Type : public Node {
+	public Node              expression;
+};
 
 // Simple nodes
-class identifier_ast {std::string   value;};
-class type_ast       {tokenType     value;};
-class number_ast     {unsigned long value;};
-class boolean_ast    {bool          value;};
-class operator_ast   {char          value;};
+class identifier_Node : public Node {std::string   value;};
+class type_Node : public Node       {tokenType     value;};
+class number_Node : public Node     {unsigned long value;};
+class boolean_Node : public Node    {bool          value;};
+class operator_Node : public Node   {char          value;};
 
-class formal_Node: public_Node {
-  identifier_ast identifier;
-  type_ast       type;
+
+
+class if_Node : public Node {
+  leftChild      then_Node : public Node;// then expression node
+  rightChild     else_Node : public Node;// else expression node
 };
 
-class if_ast {
-  leftChild      then_ast;// then expression node
-  rightChild     else_ast;// else expression node
-};
-
-class then_ast {
-    leftChild    expr_ast;//expr with boolean result
-    rightChild   expr_ast;//expr to perform if L-child is true
+class then_Node : public Node {
+    leftChild    expr_Node : public Node;//expr with boolean result
+    rightChild   expr_Node : public Node;//expr to perform if L-child is true
 };
 
 class else-ast {
-    leftChild    expr_ast;//expr with boolean result
-    rightChild   expr_ast;//expr to perform if L-child is false
-};
-
-// class Prototype
-class /*Name*/ {
-  //(Values)*
-};
-
-// Binary Prototype
-class /*name*/_ast {
-    leftChild    /*node*/_ast;
-    rightChild   /*node*/_ast;
+    leftChild    expr_Node : public Node;//expr with boolean result
+    rightChild   expr_Node : public Node;//expr to perform if L-child is false
 };
