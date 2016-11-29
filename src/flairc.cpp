c25dbbc3f685d441ae8f07ca287d2175157a4a2d
khaase@uni.edu <khaase@uni.edu>
2016-11-29 12:50:04 -0600

    Combined if statements

--- src/flairc.cpp
+++ src/flairc.cpp
@@ -18,20 +18,7 @@ int main(int argc, char *argv[]) {
       registerRo(line," OUT    ",0,0,0," #Prints 1\n");
       line += 1;
       registerRo(line," HALT   ",0,0,0," #Ends the TM code\n");
-      return 0;        
-      }
-/*
-    else if(string(argv[1]) == "print-one") {
-      //Same logic as above, but allows for the removal of .flr
-      ofstream tmFile("print-one.tm");
-      registerRm(line," LDC    ",0,1,0," #Adds one to the register\n");
-      line += 1;
-      registerRo(line," OUT    ",0,0,0," #Prints 1\n");
-      line += 1;
-      registerRo(line," HALT   ",0,0,0," #Ends the TM code\n");
-      return 0;
-      }
-*/
+      return 0; }
     else {
       //Break program on anything that is not print-one
       cout << "Whoops, you entered a file other than print-one...\n"; }}}
