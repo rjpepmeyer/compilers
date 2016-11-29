2654a9f882a0c8d0c5f5a51183580f9024e4ad31
khaase@uni.edu <khaase@uni.edu>
2016-11-28 22:00:51 -0600

    Add changes to flairc.cpp

--- src/flairc.cpp
+++ src/flairc.cpp
@@ -1,10 +1,25 @@
 #include "codegen.hpp"
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
 
-int main() {
-    line = 0;
-    registerRm(line," LDC    ",0,1,0," #Adds one to the register\n");
-    line += 1;
-    registerRo(line," OUT    ",0,0,0," #Prints 1\n");
-    line += 1;
-    registerRo(line," HALT   ",0,0,0," #Ends the TM code\n");
-    return 0; }
+int main(int argc, char *argv[]) {
+  string line;
+  ifstream inputFile(argv[1]);
+  if(inputFile) {
+    while(1) {
+      while(!inputFile.eof()) {
+        getline(inputFile,line);
+        if(line == "program main();") {
+          cout << "It's working\n";
+          return 0; }
+        else {
+          cout << "Nada\n";
+          return 0; }}}}
+
+  else {
+    cout << "Please enter 'print-one.flr' for the file name!\n";
+    return 0; }}
+   
+  
