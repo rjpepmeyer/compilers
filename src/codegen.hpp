762146ed9314df7ae398c9e32a3d7b3a20ee1322
khaase@uni.edu <khaase@uni.edu>
2016-11-29 02:58:11 -0600

    Contains our print procedure functions

--- src/codegen.hpp
+++ src/codegen.hpp
@@ -9,7 +9,11 @@ using namespace std;
 const char printOneTM[] = "0: LDC  0,1(0)\n1: OUT  0,0,0\n2: HALT 0,0,0";
 
 //PrintOneAST
-const char printOneAST = 1;
+const char printOneAST = 
+   program main();
+   begin
+     return 1
+   end.
 */
 
 //Initialize values
@@ -23,16 +27,4 @@ void registerRo(int line, string code, int r1, int r2, int r3, string comment) {
 void registerRm(int line, string code, int r1, int offset, int r2, string comment) {
     cout << line << ":" << code << r1 << "," << offset << "(" << r2 << ")" << comment;
 };
-
-/*
-//This is our print procedure
-int main() {
-    registerRm(line," LDC    ",0,1,0," #Adds one to the register\n");
-    line += 1;
-    registerRo(line," OUT    ",0,0,0," #Prints 1\n");
-    line += 1;
-    registerRo(line," HALT   ",0,0,0," #Ends the TM code\n");
-    return 0;
-};
-*/
 
