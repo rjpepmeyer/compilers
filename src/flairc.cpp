855e2f95533943da8818d83da538d6fe580e5475
khaase@uni.edu <khaase@uni.edu>
2016-11-28 22:14:36 -0600

    Having issues looping through the file to find 'return 1'

--- src/flairc.cpp
+++ src/flairc.cpp
@@ -8,18 +8,14 @@ int main(int argc, char *argv[]) {
   string line;
   ifstream inputFile(argv[1]);
   if(inputFile) {
-    while(1) {
-      while(!inputFile.eof()) {
-        getline(inputFile,line);
-        if(line == "program main();") {
-          cout << "It's working\n";
-          return 0; }
-        else {
-          cout << "Nada\n";
-          return 0; }}}}
-
+    while(getline(inputFile, line)) {
+      if(line == "return 1") {
+        cout << "It's working\n";
+        return 0; }
+      else {
+        cout << "Nada\n";
+        return 0; }}}
   else {
     cout << "Please enter 'print-one.flr' for the file name!\n";
     return 0; }}
-   
-  
+     
