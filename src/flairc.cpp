576b3e5f9d284b61f2c39bc4c61d2e1f41cc96ae
rpepmeyer <pepmeyer@uni.edu>
2016-12-03 18:56:06 -0600

    minor fixes

--- src/flairc.cpp
+++ src/flairc.cpp
@@ -1,4 +1,4 @@
-d#include <fstream>
+#include <fstream>
 #include <iostream>
 #include <string>
 #include "compiler.hpp"
@@ -20,7 +20,8 @@ int main(int argc, char *argv[]) {
   // Main Code
   if(argc > 2) {
     cout << "Usage: <./flairc> <fileName>\n";
-    return 1; }
+    return 1; 
+  }
   else {
     if((string(argv[1]) == "flair-programs/print-one.flr")||
        (string(argv[1]) == "flair-programs/print-one")) {
@@ -35,11 +36,17 @@ int main(int argc, char *argv[]) {
         registerRo(line," OUT    ",0,0,0," #Prints 1\n");
         line += 1;
         registerRo(line," HALT   ",0,0,0," #Ends the TM code\n");
-        return 0; }
+        return 0; 
+        tmFile.close();
+      }
       else {
-        return 3; }
+        return 3; 
+      }
     } else {
       //Break program on anything that is not print-one
       cout << "Whoops, you entered a file other than print-one...\n";
-      return 2; }}}
+      return 2; 
+    }
+  }
+}
     
