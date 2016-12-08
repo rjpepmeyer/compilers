94e017f9203326f4b9e801ed4c18b95adb7e9874
khaase@uni.edu <khaase@uni.edu>
2016-12-07 18:06:16 -0600

    Added namespace std

--- src/tacClass.cpp
+++ src/tacClass.cpp
@@ -1,17 +1,19 @@
 enum tacOp {add,sub,mul,div,neg,ass};
 
+using namespace std;
+
 class TAC {
   private:
     tacOp op;
-    std::string arg1;
-    std::string arg2;
-    std::string res;
+    string arg1;
+    string arg2;
+    string res;
   public:
     void setOp(tacOp in){this->op = in};
-    void set1(std::string in){this->arg1 = in};
-    void set2(std::string in){this->arg2 = in};
-    void setRes(std::string in){this->res = in};
-    std::string getRes(std::string in){
+    void set1(string in){this->arg1 = in};
+    void set2(string in){this->arg2 = in};
+    void setRes(string in){this->res = in};
+    string getRes(string in){
       switch (op){
         case ass:
           res = arg1;
